028381d61f28cc28aafab85ddecdea40b8532d44
Gizmo-30 <anvaradizov80@gmail.com>
2021-12-26 17:06:09 +0500

    first

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const hour = document.querySelector('.h'),
-    min = document.querySelector('.m'),
-    sec = document.querySelector('.s');
-
-
-
+  min = document.querySelector('.m'),
+  sec = document.querySelector('.s'),
+  hoursNumber = document.querySelector('.hours'),
+  minutesNumber = document.querySelector('.minutes');
+ 
+  
 // let time = new Date();
 
 // console.log(time);
@@ -18,47 +19,85 @@ const hour = document.querySelector('.h'),
 
 function clock() {
 
-    let time = new Date(),
-      second = time.getSeconds() * 6,
-      minutes = time.getMinutes() * 6,
+  let time = new Date(),
+    second = time.getSeconds() * 6,
+    minutes = time.getMinutes() * 6,
     hours = time.getHours() * 30;
 
-    sec.style = `transform: rotate(${second}deg)`;
-    min.style = `transform: rotate(${minutes}deg)`;
-    hour.style = `transform: rotate(${hours}deg)`;
+  sec.style = `transform: rotate(${second}deg)`;
+  min.style = `transform: rotate(${minutes}deg)`;
+  hour.style = `transform: rotate(${hours}deg)`;
 
-    setTimeout(() => clock(), 1000);
-    // Рекурсия  это когда функция вызывает саму себя внутри себя
+  hoursNumber.innerHTML = time.getHours() < 10 ? '0' + time.getHours() : time.getHours();
+  minutesNumber.innerHTML = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
+
+  setTimeout(() => clock(), 1000);
+  // Рекурсия  это когда функция вызывает саму себя внутри себя
 }
 
 clock();
 
 
-// setTimeout(() => {
-//     console.log('asdasd');
-// }, 5000)
+const tabsItem = document.querySelectorAll('.tabsItem'),
+  tabsContent = document.querySelectorAll('.tabsContentItem');
 
 
-// setInterval(() => {
-//     console.log('asdsa');
-// }, 1000)
 
-let tabsItems = document.querySelectorAll('.tabsItem')
-let tabsContentItem = document.querySelectorAll('.tabsContentItem ')
 
-tabsItems.forEach(element => {
-  element.addEventListener('click', () => {
-    removeActive(tabsItems)
-    element.classList.add('active')
-    tabsContentItem.forEach(element => {
-      removeActive(tabsContentItem)
-      element.classList.add('active')
-    });
-  });
-});
+tabsItem.forEach((item, i) => {
+  item.addEventListener('click', function (e) {
+    e.preventDefault(); /* отменяет обычное поведение элементов в браузере */
+    removeActiveClass();
+    this.classList.add('active');
+    tabsContent[i].classList.add('active');
+  })
+})
 
-function removeActive(array) {
-  array.forEach(element => {
-    element.classList.remove('active')
-  });
+function removeActiveClass() {
+  tabsItem.forEach((item, i) => {
+    item.classList.remove('active');
+    tabsContent[i].classList.remove('active');
+  })
 }
+
+const stopwatchBtn = document.querySelector('.stopwatch__btn'),
+  stopwatchSeconds = document.querySelector('.stopwatch__seconds'),
+  stopwatchMinutes = document.querySelector('.stopwatch__minutes'),
+  stopwatchHours = document.querySelector('.stopwatch__hours');
+
+function stopWatch() {
+  stopwatchSeconds.innerHTML++
+  timeOut = setTimeout(() => {
+    stopWatch()
+  }, 1000);
+  if (stopwatchSeconds.innerHTML == '60') {
+    stopwatchMinutes.innerHTML++
+    stopwatchSeconds.innerHTML = '0'
+    if (stopwatchMinutes.innerHTML == '60') {
+      stopwatchHours.innerHTML++
+      stopwatchMinutes.innerHTML = '0'
+      if (stopwatchHours.innerHTML == '24') {
+        stopwatchSeconds.innerHTML = '0'
+        stopwatchMinutes.innerHTML = '0'
+        stopwatchHours.innerHTML = '0'
+        clearTimeout(timeOut)
+        stopwatchBtn.innerHTML = 'start'
+      }
+    }
+  }
+}
+
+stopwatchBtn.addEventListener('click', () => {
+  if (stopwatchBtn.innerHTML == 'start') {
+    stopwatchBtn.innerHTML = 'stop'
+    stopWatch()
+  } else if (stopwatchBtn.innerHTML == 'stop') {
+    clearTimeout(timeOut)
+    stopwatchBtn.innerHTML = 'reset'
+  } else {
+    stopwatchSeconds.innerHTML = '0'
+    stopwatchMinutes.innerHTML = '0'
+    stopwatchHours.innerHTML = '0'
+    stopwatchBtn.innerHTML = 'start'
+  }
+})
